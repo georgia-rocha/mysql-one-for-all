9e70abc5daa69de0af7922c15ade45abd74785ae
Georgia <georgiab543@gmail.com>
2023-05-14 01:43:56 -0300

    feat: requisito 12 - cria query para mostrar a quantidade correta de canções que mais foram favoritadas de cada pessoa artista

---
+++ challenges/desafio12.sql
@@ -0,0 +1,17 @@
+SELECT
+	A.artist_name AS artista,
+	CASE
+		WHEN COUNT(F.music_id) >= '5' THEN 'A' 
+        WHEN COUNT(F.music_id) > '2' THEN 'B'
+        WHEN COUNT(F.music_id) > 0 THEN 'C'
+        ELSE '-'
+	END AS ranking
+FROM artists AS A
+INNER JOIN albums AS Alb
+ON A.artist_id = Alb.artista_id
+INNER JOIN musics AS M
+ON Alb.album_id = M.album_id
+LEFT JOIN favorites_musics AS F
+ON M.music_id = F.music_id
+GROUP BY A.artist_id
+ORDER BY COUNT(F.music_id) DESC, A.artist_name;
